ca5772618e07335e8fbb9da74b489d9620faf794
Senuth Abeywardana <senuthabeywardana@gmail.com>
2025-09-17 19:20:17 +0530

    make few pages on login section

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+export default function Home() {
+  return (
+    <div>
+        <h1>Welcome to kandypack lifestore</h1>
+        <Link to='/login'><button variant="light">Sign In</button></Link>
+        <Link to='/signup'><button variant="light">Sign Up</button></Link>
+    </div>
+  )
+}
