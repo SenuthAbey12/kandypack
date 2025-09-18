f5f4208be392c2aafcca5f7bc417c14c9df6450b
DinuuCoder <kavindamad.23@uom.lk>
2025-09-18 11:38:01 +0530

    Add Dashboard page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (
     <div>
-        <h1>Welcome to kandypack lifestore</h1>
+        <h1>Welcome to kandypack lifestore ___________</h1>
         <Link to='/login'><button variant="light">Sign In</button></Link>
         <Link to='/signup'><button variant="light">Sign Up</button></Link>
+        <Link to ='/dashboard'><button  variant="light">Dashboard</button></Link>
     </div>
   )
 }
